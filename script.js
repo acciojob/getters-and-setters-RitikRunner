e88929a84a351c682792f7369ca92f4ec7aa2ff7
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-07 02:01:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 //complete this code
 class Person {
-	constructor(name: string, age: number)
-	this.name = name;
-	this.age = age;
-}
-get name(){
-	this.name = name
-}
-set name(name: string){
-	this._name = name
-}
-get age(){
-	this.age = age;
-}
-set age(age: number){
-	this._age = age;
+  private _name: string;
+  private _age: number;
+
+  constructor(name: string, age: number) {
+    this._name = name;
+    this._age = age;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(name: string) {
+    this._name = name;
+  }
+
+  get age(): number {
+    return this._age;
+  }
+
+  set age(age: number) {
+    this._age = age;
+  }
 }
 
 class Student extends Person {
-	study(){
-	console.log(`${this.name} is studying`)
-	}
+  study(): void {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.name} is teaching`)
-	}
+  teach(): void {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
 // Do not change the code below this line
