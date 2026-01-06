b58e5817bca0b6b0b65a5b43a9faab2b6ec71b5b
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-07 01:56:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name: string, age: number)
+	this.name = name;
+	this.age = age;
+}
+get name(){
+	this.name = name
+}
+set name(name: string){
+	this._name = name
+}
+get age(){
+	this.age = age;
+}
+set age(age: number){
+	this._age = age;
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+	console.log(`${this.name} is studying`)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
